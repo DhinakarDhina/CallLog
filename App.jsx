aeb226c131661ab6512b725895a08f0b82e6d099
Dhinakar.S <146498273+DhinakarDhina@users.noreply.github.com>
2025-12-11 18:24:56 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,156 +1,113 @@
 // src/App.jsx
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
 
-import TabItem from './components/TabItem'
-import ProjectItem from './components/ProjectItem'
-import Header from './components/Header'
+import ContactItem from './components/ContactItem'
 
 import './App.css'
 
-const tabsList = [
-  {tabId: 'STATIC', displayText: 'Static'},
-  {tabId: 'RESPONSIVE', displayText: 'Responsive'},
-  {tabId: 'DYNAMIC', displayText: 'Dynamic'},
-]
-
-const projectsList = [
+const initialContactsList = [
   {
-    projectId: 0,
-    category: 'STATIC',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-s3-img.png',
-    title: 'Music Page',
-    description:
-      'The music page enables the users to browse through the images of all-time favorite music albums.',
+    id: uuidv4(),
+    name: 'Ram',
+    mobileNo: 9999988888,
+    isFavorite: false,
   },
   {
-    projectId: 1,
-    category: 'STATIC',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-s4-img.png',
-    title: 'Tourism Website',
-    description:
-      'A tourism website enables the user to browse through the images of popular destinations.',
+    id: uuidv4(),
+    name: 'Pavan',
+    mobileNo: 8888866666,
+    isFavorite: true,
   },
   {
-    projectId: 2,
-    category: 'STATIC',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-s1-img.png',
-    title: 'Advanced Technologies',
-    description:
-      'A website that gives you a basic understanding of Advanced Technologies.',
-  },
-     {
-       projectId: 3,
-       category: 'STATIC',
-       imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-s2-img.png',
-       title: 'Happy Meals',
-       description: 'Discover the best foods in over 1,000 restaurants.',
-     },
-  {
-    projectId: 4,
-    category: 'RESPONSIVE',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-r4-img.png',
-    title: 'VR Website',
-    description:
-      'VR Website enables users to explore AR and VR Products and Industry happenings.',
-  },
-  {
-    projectId: 5,
-    category: 'RESPONSIVE',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-r2-img.png',
-    title: 'Food Munch',
-    description: 'Food Much Website is a user-centric food tech website.',
-  },
-  {
-    projectId: 6,
-    category: 'RESPONSIVE',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-r3-img.png',
-    title: 'Portfolio',
-    description:
-      'A portfolio is the best alternative for a resume to showcase your skills to the digital world.',
-  },
- {
-   projectId: 7,
-   category: 'RESPONSIVE',
-   imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-r1-img.png',
-   title: 'Design',
-   description:
-     'A website to showcase the best features and give more information about the Design tool.',
- },
-  {
-    projectId: 8,
-    category: 'DYNAMIC',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-d3-img.png',
-    title: 'Speed Typing Test',
-    description:
-      'Speed Typing Test Application is capable of calculating the time to type the randomly generated quote.',
-  },
-  {
-    projectId: 9,
-    category: 'DYNAMIC',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-d1-img.png',
-    title: 'Random Joke Page',
-    description:
-      'Random Joke Page is an API-based dynamic Web Application that generates a new joke.',
-  },
-  {
-    projectId: 10,
-    category: 'DYNAMIC',
-    imageURL: 'https://assets.ccbp.in/frontend/react-js/projects-d2-img.png',
-    title: 'Sizing An Image',
-    description:
-      'This is a dynamic web application capable of adjusting the size of an element using DOM manipulations.',
+    id: uuidv4(),
+    name: 'Nikhil',
+    mobileNo: 9999955555,
+    isFavorite: false,
   },
 ]
 
 class App extends Component {
   state = {
-    activeTabId: tabsList[0].tabId,
+    contactsList: initialContactsList,
+    name: '',
+    mobileNo: '',
   }
 
-  clickTabItem = tabValue => {
-    this.setState({activeTabId: tabValue})
+  toggleIsFavorite = id => {
+    this.setState(prevState => ({
+      contactsList: prevState.contactsList.map(eachContact => {
+        if (id === eachContact.id) {
+          return {...eachContact, isFavorite: !eachContact.isFavorite}
+        }
+        return eachContact
+      }),
+    }))
   }
 
-  getFilteredProjects = () => {
-    const {activeTabId} = this.state
-    const filteredProjects = projectsList.filter(
-      eachprojectDetails => eachprojectDetails.category === activeTabId,
-    )
-    return filteredProjects
+  onAddContact = event => {
+    event.preventDefault()
+    const {name, mobileNo} = this.state
+    const newContact = {
+      id: uuidv4(),
+      name,
+      mobileNo,
+      isFavorite: false,
+    }
+
+    this.setState(prevState => ({
+      contactsList: [...prevState.contactsList, newContact],
+      name: '',
+      mobileNo: '',
+    }))
+  }
+
+  onChangeMobileNo = event => {
+    this.setState({mobileNo: event.target.value})
+  }
+
+  onChangeName = event => {
+    this.setState({name: event.target.value})
   }
 
   render() {
-    const {activeTabId} = this.state
-    const filteredProjects = this.getFilteredProjects()
+    const {name, mobileNo, contactsList} = this.state
     return (
       <div className="app-container">
-        <Header />
-        <h1 className="title">Projects</h1>
-        <p className="description">
-          Your skills and achievements showcase your strengths and abilities.
-          Speak about any new skills or software you learnt to perform the
-          project responsibilities.
-        </p>
-
-        <ul className="tabs-container">
-          {tabsList.map(tabDetails => (
-            <TabItem
-              key={tabDetails.tabId}
-              tabDetails={tabDetails}
-              clickTabItem={this.clickTabItem}
-              isActive={activeTabId === tabDetails.tabId}
+        <div className="responsive-container">
+          <h1 className="heading">Contacts</h1>
+          <form className="contact-form-container" onSubmit={this.onAddContact}>
+            <input
+              value={name}
+              onChange={this.onChangeName}
+              className="input"
+              placeholder="Name"
             />
-          ))}
-        </ul>
-
-        <ul className="project-list-container">
-          {filteredProjects.map(projectDetails => (
-            <ProjectItem
-              key={projectDetails.projectId}
-              projectDetails={projectDetails}
+            <input
+              className="input"
+              value={mobileNo}
+              onChange={this.onChangeMobileNo}
+              placeholder="Mobile Number"
             />
-          ))}
-        </ul>
+            <button type="submit" className="button">
+              Add Contact
+            </button>
+          </form>
+          <ul className="contacts-table">
+            <li className="table-header">
+              <p className="table-header-cell name-column">Name</p>
+              <hr className="separator" />
+              <p className="table-header-cell">Mobile Number</p>
+            </li>
+            {contactsList.map(eachContact => (
+              <ContactItem
+                key={eachContact.id}
+                contactDetails={eachContact}
+                toggleIsFavorite={this.toggleIsFavorite}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     )
   }
